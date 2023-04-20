ad60156b1e90d243ba715f5a85eef99a9d4b4126
theo ballet <theo@integnano-theo.c2n.u-psud.fr>
2023-04-20 15:40:54 +0200

    accel now works, simple peripheral

--- rtl/fraise_top.sv
+++ rtl/fraise_top.sv
@@ -29,21 +29,27 @@ module fraise_top #(
     output logic [DataWidth-1:0] resp_data_o, // the data read
     output logic [NbrHostsLog2-1:0] resp_ini_addr_o // the adress of the initiator of the request
 ) ;
+    // logs 
+    localparam int unsigned MatrixSizeLog2 = (MatrixSize == 1) ? 1 : $clog2(MatrixSize) ;
+    localparam int unsigned ArraySizeLog2 = (ArraySize == 1) ? 1 : $clog2(ArraySize) ;
+    
 
-    // TODO: get rid of this ready shinanigan 
-    always_comb begin : ready
-        ready_o = '1 ;
-    end
     // the accelerator resgisters adresses. 
-    localparam int unsigned OBS_START = 32'h1000 ; 
-    localparam int unsigned REG_START = 32'h0 ; 
+    localparam int unsigned REG_START = 32'h0010 ; 
     localparam int unsigned MODE_REG = 32'h000 + REG_START ; // 0 iddle, 1 inference (continuous)
-    localparam int unsigned RESULT_PTR_REG = 32'h004 + REG_START;
+    localparam int unsigned SEED_REG = 32'h004 + REG_START ; 
+    localparam int unsigned OBS_REG_1 = 32'h008 + REG_START ;
+    localparam int unsigned OBS_REG_2 = 32'h00C + REG_START ;
+    localparam int unsigned LAUNCH_REG = 32'h010 + REG_START ;
+    localparam int unsigned RES_VALID_REG = 32'h014 + REG_START ;
+    localparam int unsigned RES_REG = 32'h018 + REG_START ;
 
     logic [DataWidth-1:0] device_read_data;
-    /* verilator lint_off UNOPTFLAT */
-    logic [AddrWidth-1:0] result_ptr;
-    
+
+    // broadcast byte enable to the 32 bits
+    logic [DataWidth-1:0] req_ben_32 ;
+    assign req_ben_32 = {{8{req_ben_i[3]}}, {8{req_ben_i[2]}}, {8{req_ben_i[1]}}, {8{req_ben_i[0]}}};
+
     typedef enum logic[DataWidth-1:0] { 
         Off, 
         On
@@ -52,121 +58,155 @@ module fraise_top #(
     /* verilator lint_off UNOPTFLAT */
     mode_reg_e mode_reg ; 
 
-    localparam int unsigned MatrixSizeLog2 = (MatrixSize == 1) ? 1 : $clog2(MatrixSize) ;
-    localparam int unsigned ArraySizeLog2 = (ArraySize == 1) ? 1 : $clog2(ArraySize) ;
-    logic [MatrixSizeLog2-1:0] matrix_column ; 
-    logic [MatrixSizeLog2-1:0] matrix_line ; 
-    logic [ArraySizeLog2-1:0] array_column ;
-    logic [ArraySizeLog2-1:0] array_line ;
-    logic [MatrixSize-1:0][ArraySizeLog2 + 3-1:0] Observation_vec ; // the vector of observations
+    logic [DataWidth-1:0] obs_reg_1 ;
+    logic [DataWidth-1:0] obs_reg_2 ;
+    logic [MatrixSize-1:0][8:0]Observation_vec ;
+    assign Observation_vec = {obs_reg_2[24:16], obs_reg_2[8:0], obs_reg_1[24:16], obs_reg_1[8:0]} ; 
+    logic launch_reg ; 
+    logic res_valid ; 
+    logic stay_high ;
+
+    logic [MatrixSize-1:0][2**Nword_used-1:0] results ;
 
     // function to decode the input of the accelerator 
     always_ff @(posedge(clk_i)) begin : ReadInput
         if(req_valid_i) begin
             case(req_addr_i)
-                RESULT_PTR_REG: begin
-                    
-                    if(req_wen_i) begin
-                        /* verilator lint_off ALWCOMBORDER */
-                        result_ptr = req_wdata_i ; //TODO : respect BEN
-                    end 
-                    device_read_data = result_ptr;
-                end
-                MODE_REG: begin
-                    
+                MODE_REG: begin 
                     if(req_wen_i) begin
                         /* verilator lint_off ALWCOMBORDER */
                         mode_reg = mode_reg_e'(req_wdata_i) ;
                     end
                     device_read_data = mode_reg;
+                end 
+                SEED_REG: begin 
+                    if(req_wen_i) begin
+                        seeds = req_wdata_i[2**Nword_used-1:0];
+                        // TODO: write seeds 
+                    end 
+                    device_read_data = {24'b0, seeds} ;
+                end
+                OBS_REG_1: begin 
+                    if(req_wen_i) begin
+                        obs_reg_1 = req_wdata_i & req_ben_32 ;
+                    end
+                    device_read_data = obs_reg_1 ;
+                end
+                OBS_REG_2: begin 
+                    if(req_wen_i) begin
+                        obs_reg_2 = req_wdata_i & req_ben_32 ;
+                    end
+                    device_read_data = obs_reg_2 ;
+                end
+                LAUNCH_REG: begin 
+                    if(req_wen_i) begin
+                        launch_reg <= req_wdata_i[0] ;
+                    end
+                    device_read_data = {31'b0, launch_reg} ;
+                end 
+                RES_VALID_REG: begin 
+                    device_read_data = {31'b0, res_valid} ;
+                end
+                RES_REG: begin 
+                    device_read_data = results ;
+                    res_valid <= '0 ;
                 end
                 default: begin
-                    // add error flag in verilator
-
-                    // sel if in array or in observations 
-                    // for now everything is an obs at this point
-                    if(req_wen_i) begin
-                        // an observation takes 2Bytes 
-                        Observation_vec[32'(req_addr_i[MatrixSizeLog2:1])>>1] = req_wdata_i[ArraySizeLog2 + 3-1:0];
-                    end
+                    `ifdef VERILATOR 
+                        $display("FRAISE ERROR : addres out of range of the control registers : %h", req_addr_i);
+                    `endif 
                 end
             endcase
             resp_valid_o = 1'b1; 
             resp_ini_addr_o = req_host_addr_i; 
             resp_data_o = device_read_data;
+            stay_high = 1'b1 ;
         end else begin
+            if(stay_high ) begin 
+                stay_high ='0 ;
+                resp_valid_o = 1'b1;
+            end else begin
             resp_valid_o = 1'b0;
+            end
+
+
         end
     end
 
-    // state machine for taking in observations 
-    
+    // inference 
+
     typedef enum int { 
         Idle,
-        send_obs_addr,
-        average_output, // averages the output of the array (full stoch for now)
-        send_obs_data
-     } observation_state_e;    
+        Write_obs, 
+        Run, 
+        Done
+     } inference_state_e;
 
-    observation_state_e observation_state = Idle; 
-    logic [MatrixSizeLog2-1:0] compteur_observation ; 
-    logic count_obs_reset, count_obs_enable;
-    logic [Nword_used-1:0] compteur_result ;
-    logic count_result_reset, count_result_enable;
-    logic [Nword_used**2-1:0] result ;
+    inference_state_e inference_state ; 
+    genvar i ;
 
-    always_ff @( posedge(clk_i) ) begin : Inference
-        case(observation_state) 
+    always_ff @( posedge(clk_i) ) begin : Inference_machine
+        case(inference_state)
             Idle: begin
-                count_obs_reset = 1'b1;
-                if(mode_reg == On) begin
-                    observation_state = send_obs_addr;
+                counter_reset = '0 ;
+                if(mode_reg == On | launch_reg == '1) begin
+                    inference_state = Write_obs;
+                end 
+            end
+            Write_obs: begin
+                ready_o <= '0 ; 
+                launch_reg <= '0 ; 
+                counter_en <= '1 ; 
+
+                inference <= '1 ;
+                addr_col <= {counter[1:0],Observation_vec[counter[1:0]][2:0], 3'b0} ; 
+                addr_row <= {2'b0, Observation_vec[counter[1:0]][ArraySizeLog2 + 3 - 1:3]} ;
+
+                if(counter == 8'(MatrixSize-1)) begin
+                    counter_reset = '1 ;
+                    inference_state = Run;
+                end
+
+            end
+            Run: begin
+                ready_o <= '1 ;
+                inference <= '0 ;
+                counter_reset = '0 ;
+                counter_en <= '1 ;
+
+                results[0] <= results[0] + 8'(bit_out[0]) ;
+                results[1] <= results[1] + 8'(bit_out[1]) ;
+                results[2] <= results[2] + 8'(bit_out[2]) ;
+                results[3] <= results[3] + 8'(bit_out[3]) ;
+
+                if(counter >= (2**8)-1) begin
+                    counter_en <= '0 ;
+                    inference_state = Done;
                 end
             end
-            send_obs_addr: begin
-                count_obs_reset = 1'b0;
-                count_obs_enable = 1'b1; 
-                inference = 1'b1;
-                count_result_reset = 1'b1;
-
-                addr_col = {compteur_observation,Observation_vec[compteur_observation][2:0], 3'b0} ; 
-                addr_row = {2'b0, Observation_vec[compteur_observation][ArraySizeLog2 + 3 - 1:3]} ;
-
-                if(compteur_observation == 2'(ArraySize-1)) begin
-                    observation_state = average_output;
-                end
+            Done: begin
+                counter_reset = '1 ;
+                res_valid <= '1 ; 
+                inference_state = Idle;
             end
-            average_output: begin
-                count_obs_enable = 1'b0;
-                count_result_reset = 1'b0;
-                count_result_enable = 1'b1;
-
-                result = (result + 8'(bit_out)); 
-
-                if(compteur_result == 3'(Nword_used-1)) begin
-                    observation_state = send_obs_data;
-                end
-            end
-            send_obs_data : begin
-                count_result_enable = 1'b0;
-                
-                if(compteur_result == 3'(Nword_used-1)) begin
-                    observation_state = Idle;
-                end
-            end
-
         endcase
-        
     end
 
-    always_ff @( posedge(clk_i) ) begin : count
-        if(count_obs_reset) begin
-            compteur_observation <= 0;
-        end
-        else if(count_obs_enable) begin
-            compteur_observation <= compteur_observation + 1;
+    // counter 
+
+    logic [2**Nword_used-1:0] counter ;
+    logic counter_reset, counter_en ; 
+
+    always_ff @( posedge(clk_i) ) begin : compteur
+        if(counter_reset) begin
+            counter = '0 ; 
+        end else if(counter_en) begin
+            counter = counter + 1 ; 
         end
     end
+
+
+    // interface with Bayesian_stoch_log TODO: add a proper interface 
 
     logic [ArraySizeLog2 + MatrixSizeLog2 -1 :0] addr_col ;
     logic [ArraySizeLog2 + MatrixSizeLog2 -1 :0] addr_row ; 
@@ -177,7 +217,7 @@ module fraise_top #(
     Bayesian_stoch_log #(
         .Narray(MatrixSizeLog2),
         .Nword(ArraySizeLog2),
-        .Nword_used(Nword_used), 
+        .Nword_used(Nword_used)
     ) e_Bayesian_stoch_log (
         .clk(clk_i),
         .CBL(1'b0),
@@ -199,5 +239,28 @@ module fraise_top #(
         .bit_out(bit_out)
     ) ;
     
+    // reset management 
+
+    always_ff @(posedge(clk_i)) begin : Reset_management
+        if(!reset_n) begin
+            inference_state = Idle;
+            counter_reset = '0 ;
+            counter_en <= '0 ;
+            mode_reg = Off ;
+            launch_reg <= '0 ;
+            res_valid <= '0 ;
+            ready_o <= '1 ;
+            inference <= '0 ;
+            load_seed <= '0 ;
+            read_1 <= '0 ;
+            read_8 <= '0 ;
+            load_mem <= '0 ;
+            read_out <= '0 ;
+            obs_reg_1 = '0 ;
+            obs_reg_2 = '0 ;
+        end
+    end
+
+
 
 endmodule
